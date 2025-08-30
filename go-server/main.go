1dffa22f5950f12084595bf8a3d78c237a7a24a2
Doraemon cyber team <117832562+Baba01hacker666@users.noreply.github.com>
2025-08-30 08:53:18 +0545

    Add files via upload

--- go-server/main.go
+++ go-server/main.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	addr := ":8080"
+	log.Printf("Serving static files on http://localhost%s\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
